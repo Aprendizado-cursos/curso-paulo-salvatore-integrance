45af4d2e68aca1f386d9a410bcd47fd8d2fe3de9
Leonardo Petta do Nascimento <leonardo.petta@integrance.com.br>
2021-08-07 12:03:17 -0300

    feat(colaborador): Implementation create and update

--- node-nest/src/colaboradores/colaboradores.service.ts
+++ node-nest/src/colaboradores/colaboradores.service.ts
@@ -4,6 +4,7 @@ import { EntityNotFoundError } from 'src/errors/entity-not-found.error';
 import { CreateColaboradorDto } from './dto/create-colaborador.dto';
 import { UpdateColaboradorDto } from './dto/update-colaborador.dto';
 import { Colaborador } from './entities/colaborador.entity';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ColaboradoresService {
@@ -22,14 +23,20 @@ export class ColaboradoresService {
   ];
 
   create(createColaboradorDto: CreateColaboradorDto) {
-    const item: Colaborador = {
-      id: this.data.length + 1,
+    // const item: Colaborador = {
+    //   id: this.data.length + 1,
+    //   ...createColaboradorDto,
+    // };
+
+    // this.data.push(item);
+
+    // return item;
+
+    const data: Prisma.colaboradorCreateInput = {
       ...createColaboradorDto,
     };
 
-    this.data.push(item);
-
-    return item;
+    return this.prisma.colaborador.create({ data });
   }
 
   findAll() {
@@ -37,8 +44,6 @@ export class ColaboradoresService {
   }
 
   findOne(id: number) {
-    const index = this.findIndexById(id);
-
     return this.prisma.colaborador.findMany({
       where: {
         id,
@@ -47,14 +52,20 @@ export class ColaboradoresService {
   }
 
   update(id: number, updateColaboradorDto: UpdateColaboradorDto) {
-    const index = this.findIndexById(id);
+    // const index = this.findIndexById(id);
 
-    this.data[index] = {
-      ...this.data[index],
+    // this.data[index] = {
+    //   ...this.data[index],
+    //   ...updateColaboradorDto,
+    // };
+
+    // return this.data[index];
+
+    const data: Prisma.colaboradorUpdateInput = {
       ...updateColaboradorDto,
     };
 
-    return this.data[index];
+    return this.prisma.colaborador.update({ where: { id }, data });
   }
 
   remove(id: number) {
