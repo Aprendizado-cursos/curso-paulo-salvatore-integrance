1a2752086bd97788c67130db3db12c260c35c0b0
Leonardo Petta do Nascimento <leonardo.petta@integrance.com.br>
2021-08-10 16:25:58 -0300

    feat(Empresa): :sparkles: Add empresa resource

--- node-nest/src/app.module.ts
+++ node-nest/src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ColaboradoresModule } from './colaboradores/colaboradores.module';
+import { EmpresasModule } from './empresas/empresas.module';
 @Module({
-  imports: [ColaboradoresModule, PrismaModule],
+  imports: [ColaboradoresModule, PrismaModule, EmpresasModule],
   controllers: [AppController],
   providers: [AppService],
 })
