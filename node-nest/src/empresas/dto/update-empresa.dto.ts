7bd38901b8900bf1f3d30769b6105da90dedfb7d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-08-11 11:36:02 -0300

    feat(Empresa): :pencil2: Retirando importações sem necessidade

--- node-nest/src/empresas/dto/update-empresa.dto.ts
+++ node-nest/src/empresas/dto/update-empresa.dto.ts
@@ -1,9 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { IsString } from 'class-validator';
 import { Colaborador } from 'src/colaboradores/entities/colaborador.entity';
 import { Empresa } from '../entities/empresa.entity';
-import { CreateEmpresaDto } from './create-empresa.dto';
-
 export class UpdateEmpresaDto implements Empresa {
   @IsString()
   razaoSocial: string;
