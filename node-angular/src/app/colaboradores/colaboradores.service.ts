9a1aea4bc023bf330f3c12e00b7d1fc58a6388b5
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-08-12 13:01:40 -0300

    feat(colaboradores): implement component and service

--- node-angular/src/app/colaboradores/colaboradores.service.ts
+++ node-angular/src/app/colaboradores/colaboradores.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export type Colaboradores = {
+  id: number;
+  nome: string;
+  idade: number | null;
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColaboradoresService {
+  public colaboradores: Colaboradores[] = [];
+  constructor(private readonly httpClient: HttpClient) {
+    this.loadColaboradores();
+  }
 
-  constructor() { }
+  async loadColaboradores() {
+    const urlApi = 'http://localhost:3000/colaboradores/';
+    this.colaboradores = await this.httpClient
+      .get<Colaboradores[]>(urlApi)
+      .toPromise();
+  }
 }
--- node-nest/src/main.ts
+++ node-nest/src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 import { NotFoundInterceptor } from './interceptors/not-found.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:4200',
+      methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+      credentials: true,
+    },
+  });
 
   // Pipes
   app.useGlobalPipes(
